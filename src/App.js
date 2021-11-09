a7a5b3a9dbdc7a0e674f0028cd2c365695eb35f2
Lana Ferrari <ferrarirangel@gmail.com>
2021-11-09 18:56:40 +0000

    Creating files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
+import { useEffect, useState } from "react";
+import { Route, Switch } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./Pages/Home";
+import LoginForm from "./Pages/Login Form";
+import ProjectDetails from "./Pages/Project Details";
+import UserHome from "./Pages/User Home";
+import UserProjectForm from "./Pages/User Prjct Form";
+import UserProjects from "./Pages/User Projects";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/login" exact>
+        <LoginForm />
+      </Route>
+      <Route exact path="/project/:id/:name">
+        <ProjectDetails />
+      </Route>
+      <Route path="/:userName" exact>
+        <UserHome />
+      </Route>
+      <Route path="/:userName/create-project" exact>
+        <UserProjectForm />
+      </Route>
+      <Route path="/:userName/myProjects" exact>
+        <UserProjects />
+      </Route>
+    </Switch>
   );
 }
 
