b5392a432b4b0902b01058660ed96f35b3dbeae5
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-11-10 19:24:12 +0000

    Cleanup: remove react logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Pages/Home";
 import LoginForm from "./Pages/LoginForm";
